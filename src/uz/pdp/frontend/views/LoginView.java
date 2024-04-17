77a03cc7bec34cbe5256c6c6d77af0fd81adbf36
Sanobar <arslonboyevasanobar@.gmail.com>
2024-04-17 13:36:52 +0500

    Login

--- src/uz/pdp/frontend/views/LoginView.java
+++ src/uz/pdp/frontend/views/LoginView.java
@@ -1,4 +1,24 @@
 package uz.pdp.frontend.views;
 
+import uz.pdp.backend.dto.LoginDTO;
+import uz.pdp.backend.entity.user.User;
+import uz.pdp.backend.service.userservice.UserService;
+import uz.pdp.backend.service.userservice.UserServiceImplementation;
+import uz.pdp.frontend.utils.ScanUtil;
+
 public class LoginView {
+    private static UserService userService = UserServiceImplementation.getInstance();
+
+    public static User login(){
+        System.out.println("Enter login info");
+        String username = ScanUtil.scanString("Enter username: ");
+        String password = ScanUtil.scanString("Enter password: ");
+        LoginDTO loginDTO = new LoginDTO(username, password);
+        return userService.login(loginDTO);
+    }
+    public static void signUp(){
+//        userService.create(new User(ScanUtil.scanString("Enter fullName: "),
+//                ScanUtil.scanString("Enter username: "),
+//                ScanUtil.scanString("Enter password: ")));
+    }
 }
