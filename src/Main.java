4382cb2a0e56c3c63a8a94a7826867d8aee5b4d8
Ozoda <ozodaxonzoda05@gmail.com>
2024-04-19 00:29:23 +0500

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import uz.pdp.backend.entity.user.User;
+import uz.pdp.frontend.utils.MenuUtils;
+import uz.pdp.frontend.views.LoginView;
+import uz.pdp.frontend.views.UserView;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("*** WELCOME TO TELEGRAM ***");
 
+        while (true){
+            switch (MenuUtils.menu(MenuUtils.LOGIN)){
+                case 1->{ User loginUser = LoginView.login();
+                    if (loginUser!=null){
+                            UserView.profile(loginUser);
+                        } else {
+                        System.out.println("Your password or login incorrect❌❌❌");
+                    }
+                }
+                case 2->LoginView.signUp();
+                case 3->{
+                    System.out.println("Bye bye");
+                    System.exit(0);
+                }
+            }
+        }
     }
 }
--- src/uz/pdp/frontend/views/UserView.java
+++ src/uz/pdp/frontend/views/UserView.java
@@ -1,4 +1,37 @@
 package uz.pdp.frontend.views;
 
+import uz.pdp.backend.entity.user.User;
+import uz.pdp.frontend.utils.MenuUtils;
+
 public class UserView {
+    private static User currentUser;
+    public static void profile(User user) {
+        currentUser = user;
+        while (true){
+            switch (MenuUtils.menu(MenuUtils.USER)){
+                case 1->showMyChats();
+                case 2->showMyGroup();
+                case 3->showMyContact();
+                case 4->showMyProfile();
+                case 0->{
+                    currentUser = null;
+                    System.out.println("Logging out");
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void showMyProfile() {
+    }
+
+    private static void showMyContact() {
+    }
+
+    private static void showMyGroup() {
+    }
+
+    private static void showMyChats() {
+    }
+
 }
