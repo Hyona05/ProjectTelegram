35b334fd814fd9aa1d868eac874810dc6625799b
Shahnoza2013 <davlatergoziev78@gmail.com>
2024-04-19 00:29:38 +0500

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import uz.pdp.backend.entity.user.User;
 import uz.pdp.frontend.utils.MenuUtils;
 import uz.pdp.frontend.utils.ScanUtil;
+import uz.pdp.frontend.views.LoginView;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +10,7 @@ public class Main {
             int menu = MenuUtils.menu(MenuUtils.LOGIN);
             switch (menu){
                 case 1 -> signIn();
-                case 2 -> SignUp();
+                case 2 -> signUp();
                 case 3 -> {
                     System.out.println("Come again. Bye bye!");
                     return;
@@ -17,7 +19,11 @@ public class Main {
         }
     }
 
-    private static void SignUp() {
+    private static void signUp() {
+        User login = LoginView.login();
+        if(login!=null){
+
+        }
     }
 
     private static void signIn() {
