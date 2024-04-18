26c5620afd8be224519e9a7f904da1d14241bf99
Shahnoza2013 <davlatergoziev78@gmail.com>
2024-04-18 22:53:18 +0500

    messageServiceImp is ready

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import uz.pdp.frontend.utils.MenuUtils;
+import uz.pdp.frontend.utils.ScanUtil;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("Welcome to our TELEGRAM app!!!");
+        while (true) {
+            String login = MenuUtils.LOGIN;
+            switch (ScanUtil.scanInt("Choose action: ")) {
+                case 1 -> signIn();
+                case 2 -> SignUp();
+                case 3 -> {
+                    System.out.println("Come again. Bye bye!");
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void SignUp() {
+    }
+
+    private static void signIn() {
 
     }
 }
