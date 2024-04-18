73c49b043fdd0f5d15548c7d1aba49ec741835b4
Ozoda <ozodaxonzoda05@gmail.com>
2024-04-19 00:18:43 +0500

    group implementation

--- src/uz/pdp/backend/service/groupservice/GroupServiceImplementation.java
+++ src/uz/pdp/backend/service/groupservice/GroupServiceImplementation.java
@@ -14,27 +14,45 @@ public class GroupServiceImplementation implements GroupService{
 
     @Override
     public void create(Group entity) {
-
+     groups.add(entity);
+        System.out.println("Group created");
     }
 
     @Override
     public Group get(String id) {
+        for (Group group : groups) {
+            if (group.getId().equals(id)) {
+                return group;
+            }
+        }
         return null;
     }
 
     @Override
     public List<Group> getList() {
-        return null;
+        return groups;
     }
 
     @Override
     public void update(Group newEntity) {
-
+        for (int i = 0; i < groups.size(); i++) {
+            if (groups.get(i).getId().equals(newEntity.getId())) {
+                groups.set(i, newEntity);
+                System.out.println("Group updated");
+                return;
+            }
+        }
     }
 
     @Override
     public void delete(String id) {
-
+     for (Group group : groups) {
+         if (group.getId().equals(id)) {
+             groups.remove(group);
+             System.out.println("Group deleted");
+             return;
+         }
+      }
     }
 
    private static GroupServiceImplementation groupServiceImpl;
