91eaf2928a2ac97692d42dab86583cfc179db09d
Ozoda <ozodaxonzoda05@gmail.com>
2024-04-18 22:43:48 +0500

    sign up

--- src/uz/pdp/backend/service/userservice/UserServiceImplementation.java
+++ src/uz/pdp/backend/service/userservice/UserServiceImplementation.java
@@ -7,6 +7,7 @@ import uz.pdp.backend.entity.user.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class UserServiceImplementation implements UserService{
     private List<User> list;
@@ -21,26 +22,45 @@ public class UserServiceImplementation implements UserService{
 
     @Override
     public User get(String id) {
+        for (User user : list) {
+            if (user.getId().equals(id)) {
+                return user;
+            }
+        }
         return null;
     }
 
     @Override
     public List<User> getList() {
-        return null;
+        return list;
     }
 
     @Override
     public void update(User newEntity) {
-
+        for (int i = 0; i < list.size(); i++) {
+            User user = list.get(i);
+            if (user.getId().equals(newEntity.getId())) {
+                list.set(i, newEntity);
+                System.out.println("User updated");
+                return;
+            }
+        }
+        System.out.println("User not found");
     }
+
 
     @Override
     public void delete(String id) {
-
+      for (User user: list) {
+          if (user.getId().equals(id)) {
+              list.remove(user);
+          }
+      }
+        System.out.println("User deleted: " + id);
     }
 
     @Override
-    public User login(LoginDTO login) {
+    public User login(LoginDTO login){
         return null;
     }
 
