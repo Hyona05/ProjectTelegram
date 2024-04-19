fffb79a6876c8818923fafc853a7865c8b4c8768
Shahnoza2013 <davlatergoziev78@gmail.com>
2024-04-19 12:50:12 +0500

    main

--- src/uz/pdp/backend/service/charservice/ChatService.java
+++
@@ -1,7 +0,0 @@
-package uz.pdp.backend.service.charservice;
-
-import uz.pdp.backend.entity.chat.Chat;
-import uz.pdp.backend.service.BaseService;
-
-public interface ChatService extends BaseService<Chat> {
-}
